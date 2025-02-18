44737580623b744d11080c1576fb6768a34b27bb
Heitor <haoramoss@gmail.com>
2025-02-17 23:54:40 -0300

    feat: added code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {server} from './server/server'
+import { server } from './server/server';
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`Running on port: ${port}`);
-})
+  console.log(`Running on port: ${port}`);
+});
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 
 const server = express();
 
-server.get('/', (req : Request, res : Response) => {
-    res.json({message: 'Hello, World!'});
-    return;
+server.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello, World!' });
+  return;
 });
 
-export {server};
+export { server };
